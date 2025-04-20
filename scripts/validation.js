2f7cfe97c3a2b64fd941fcd14331c29f9fd80264
abedag <abedalrahman7117@gmail.com>
2025-04-20 17:38:33 +0300

    Add <validation.js> Scripting errors with messages

--- scripts/validation.js
+++ scripts/validation.js
@@ -14,7 +14,34 @@ if (form) {
 
     let errors = [];
 
-    
+    if (username_input && confirm_pass_input) {
+
+      errors = getSignupFormErrors(
+        email_input.value,
+        username_input.value,
+        pass_input.value,
+        confirm_pass_input.value
+      );
+
+      if (errors.length === 0) {
+        const user = {
+          email: email_input.value,
+          username: username_input.value,
+          password: pass_input.value,
+        }
+
+        localStorage.setItem('quizUser', JSON.stringify(user));
+
+        error_message.className = 'message success';
+        error_message.innerText = "✅ Registration successful! Redirecting to login...";
+        setTimeout(() => {
+          window.location.href = "/index.html";
+        }, 2000);
+      } else {
+        showErrors(errors);
+      }
+
+    }
   })
 }
 
